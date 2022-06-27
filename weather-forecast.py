4f69ff588bcdb2d928ddf02d76cfd53afb7876ae
Ivan Santos <ivancsantos@hotmail.com.br>
2022-06-27 11:58:11 -0300

    Adjustments. API data language set to pt_pr and units of measurement set to metric system

--- weather-forecast.py
+++ weather-forecast.py
@@ -1,48 +1,38 @@
 import requests
 import json
 
-
 location = input("Informe o nome da cidade que deseja a localização: ")
 # Geocoding API
 # https://openweathermap.org/api/geocoding-api
 # http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 API_GEO = "http://api.openweathermap.org/geo/1.0/direct"
+API_KEY="5253fb912cce5041e499f7a27f4cad17"
 parameters = {
   "q" : location,
-  "limit" : 5,
-  "appid" : "5253fb912cce5041e499f7a27f4cad17"
+  "limit" : 1,
+  "appid" : API_KEY
 }
 response = requests.get(API_GEO, params=parameters)
-print(response.status_code)
-#print(response.json())
-#print(json.dumps(response.json(), indent=4))
-#print(json.dumps(response.json()[0]["lat"], indent=4))
-#response = json.dumps(response.json(), indent=4)
-#print(response.json())
-latitude = response.json()[0]['lat']
-longitude = response.json()[0]['lon']
-# Itajai:
+if(response.status_code == 200):
+  latitude = response.json()[0]['lat']
+  longitude = response.json()[0]['lon']
+
+# Dados para Itajai / SC:
 # https://www.google.com.br/maps/@-26.9162788,-48.6665522,16.47z
 print("Latitude: " + str(latitude))
 print("Longitude: " + str(longitude))
 
-
 # Call curent weather data
 # https://openweathermap.org/current
 # https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-#lat="latitude"
-#lon="longitude"
-#API_KEY="key"
-#API = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY
 API_OWM = "https://api.openweathermap.org/data/2.5/weather"
 parameters = {
-  "lat" : "latitude",
-  "lon" : "longitude",
-  "appid" : "5253fb912cce5041e499f7a27f4cad17"
+  "lat" : latitude,
+  "lon" : longitude,
+  "appid" : API_KEY,
+  "lang" : "pt_br",
+  "units" : "metric"
 }
-#print(API)
-
-#response = requests.get(API_OWM, params=parameters)
-#print(response.status_code)
-#print(response.json())
-#print(json.dumps(response.json(), indent=4))
+response = requests.get(API_OWM, params=parameters)
+print(response.status_code)
+print(json.dumps(response.json(), indent=4))
